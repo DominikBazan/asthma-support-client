098ac10755434d16a10fe5f01ae1294d4e63d82d
DominikBazan <dominik.f.bazan@gmail.com>
2020-09-22 19:04:37 +0200

    Registration encryption

--- app/src/main/java/agh/asthmasupport/global/GlobalStorage.java
+++ app/src/main/java/agh/asthmasupport/global/GlobalStorage.java
@@ -2,13 +2,9 @@ package agh.asthmasupport.global;
 
 public class GlobalStorage {
 
-//    public static final String baseUrl = "192.168.43.136/";
-//    public static final String baseUrl = "192.168.43.136:5000/";
-//    public static final String baseUrl = "192.168.11.12:5000/";
-    public static final String baseUrl = "https://asthma-support-web-service.herokuapp.com/";
+//    public static final String baseUrl = "https://asthma-support-web-service.herokuapp.com/";
+    public static final String baseUrl = "https://https://app-name-cezar1.herokuapp.com/";
 //    public static final String baseUrl = "http://localhost:5000/";
-//    public static final String baseUrl = "192.168.11.12/";
-//    public static final String routeLogin = "login";
 
     public static String email;
     public static String password;
